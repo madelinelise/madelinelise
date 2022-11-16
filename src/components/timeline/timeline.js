d006a8aafd4859d6afa5b0aa062bf78a99938225
madelinelise <madelinelise@gmail.com>
2022-11-15 19:42:12 -0800

    Updates color scheme.

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -6,12 +6,12 @@ export const Timeline = (props) => {
   const { jobs } = props;
   const listItems = jobs.map((job) => (
     <li className="timeline-item">
-      <div className="timeline-info">
-        <span>{job.dates}</span>
-      </div>
       <div className="timeline-marker"></div>
       <div className="timeline-content">
         <h3 className="timeline-title">{job.title}</h3>
+        <div className="timeline-info">
+        <span>{job.dates}</span>
+      </div>
         <h3 className="timeline-company">{job.company}</h3>
         { job.descs && <List items={job.descs} /> }
       </div>
